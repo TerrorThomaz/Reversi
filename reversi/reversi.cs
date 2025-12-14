fafde885a40693e2b319c9e085220ad0c2930e6d
TerrorThomaz <thomaskoemans1@gmail.com>
2025-12-14 14:13:32 +0100

    reversi matrix

--- reversi/reversi.cs
+++ reversi/reversi.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.Metrics;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 Form scherm = new Form();
@@ -9,12 +11,35 @@ scherm.ClientSize = new Size(220, 220);
 // met een Bitmap kun je een plaatje opslaan in het geheugen
 Bitmap plaatje = new Bitmap(200, 200);
 
-// je kunt de losse pixels van het plaatje manipuleren
-plaatje.SetPixel(10, 10, Color.Red);
+byte[,] matrix(int n)
+{
+    byte[,] matrix = new byte [n, n];
+    int middle = (int)(n / 2);
+    int color = 0;
 
-// maar om complexere figuren te tekenen heb je een Graphics nodig
-Graphics tekenaar = Graphics.FromImage(plaatje);
-tekenaar.FillEllipse(Brushes.Blue, 30, 40, 100, 50);
+    for(int i = -1; i < 1;i++)
+    {
+        for (int j = -1; j < 1; j++)
+        {
+            color++;
+            int base_color = (color % 2) +1;
+            if (i == 0) base_color = 3 - base_color;
+            matrix[middle + i, middle + j] = (byte)base_color;
+
+
+        }
+        
+    }
+    
+
+
+    return matrix;
+}
+byte[,] myArray =matrix(4);
+
+Label Test = new Label();
+scherm.Controls.Add(Test);
+Test.Text = myArray[0,1].ToString();
 
 // een Label kan ook gebruikt worden om een Bitmap te laten zien
 Label afbeelding = new Label();
