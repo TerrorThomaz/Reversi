d743f19844758369e70b1d7665647260160995f4
TerrorThomaz <thomaskoemans1@gmail.com>
2025-12-21 18:12:04 +0100

    heb comments toegevoegd

--- reversi/reversi.cs
+++ reversi/reversi.cs
@@ -61,42 +61,47 @@ bool InBounds(int r, int c)
 
  byte[,] legal()
 {
+    //pakt lengte en breedte van het veld
     int rows = myArray.GetLength(0);
     int cols = myArray.GetLength(1);
-
+    //maak array aan
     byte[,] legal_array = new byte[rows, cols];
-
+    //check voor elke speler
     for (byte player = 1; player <= 2; player++)
-    {
+    {   
+        //zelfde logica als speler maar dan omgekeert
         byte opponent = (player == 1) ? (byte)2 : (byte)1;
-
+        //gaat langs elke cel
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++)
             {
+                //als cel leeg is dan slaat hij over
                 if (myArray[i, j] != 0)
                     continue;
-
+                //zo niet elke windrichting checken
                 for (int dx = -1; dx <= 1; dx++)
                     for (int dy = -1; dy <= 1; dy++)
                     {
+                        //als windrichting 0,0 is dan slaat hij over
                         if (dx == 0 && dy == 0) continue;
-
+                        //coordinaat + richting
                         int x = i + dx;
                         int y = j + dy;
-
+                        //als het speler is of 0 of out of bounds dan slaat hij over
                         if (!InBounds(x, y) || myArray[x, y] != opponent)
                             continue;
 
                         x += dx;
                         y += dy;
-
+                        //terwijl het in bounds is
                         while (InBounds(x, y))
                         {
+                            //stop bij 0
                             if (myArray[x, y] == 0)
                                 break;
-
+                            //wanneer het speler is
                             if (myArray[x, y] == player)
-                            {
+                            {   //zet in legale zetten array voor wie het legaal is
                                 legal_array[i, j] |= player; // 1 or 2
                                 break;
                             }
@@ -113,24 +118,33 @@ bool InBounds(int r, int c)
 
 void flipper(int x, int y, byte player)
 {
+    //zet huidige vak naar speler
     myArray[x, y] = player;
+    //zelfde logica als speler maar dan andersom
     byte opponent = (player == 1) ? (byte)2 : (byte)1;
+    //voor elke windrichting
     for (int dx = -1; dx <= 1; dx++) for (int dy = -1; dy <= 1; dy++)
         {
+            //slaat 0,0 over
             if (dx == 0 && dy == 0) continue;
+            //coordinaat = beginpunt + richting
             int Cor_x = x + dx;
             int Cor_y = y + dy;
+            //als de coordinaat waar het opkomt niet een tegenstander is dan slaat hij over
             if (!InBounds(Cor_x, Cor_y) || myArray[Cor_x, Cor_y] != opponent) continue;
 
+            //neemt stappen naar voren terwijl het nog tegenstander is
             while (InBounds(Cor_x, Cor_y) && myArray[Cor_x, Cor_y] == opponent)
             {
                 Cor_x += dx; Cor_y += dy;
             }
+            //als het buiten bounds is of het is de tegenstander of niks dan stopt het
             if (!InBounds(Cor_x, Cor_y) || myArray[Cor_x, Cor_y] != player)
                 continue;
+            //stap terug naar laatste tegenstander
             Cor_x -= dx; Cor_y -= dy;
 
-
+            //loopt terug tot begin punt terwijl het alles meeverandert
                     while (Cor_x != x || Cor_y != y)
                     {
                         myArray[Cor_x, Cor_y] = player;
